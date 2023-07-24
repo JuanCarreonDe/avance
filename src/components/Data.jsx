0d2f56b27333e1a86546c87144a5700d2e6e8e23
Juan Carreon <juancarreondelira@gmail.com>
2023-07-24 11:39:29 -0600

    close data

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,26 +1,33 @@
-const Data = ({ models, active }) => {
-  return (
-    //   <div className="escene__data data">
-    <>
-      {models.map((model) => (
-        <div
-          key={model.id}
-          className={`${
-            active === model.id ? "escene__data--active" : ""
-          } escene__data`}
-          onClick={() => handleButtonClick(model)}
-        >
-          <span
-            className="data__span"
-          >
-            {model.title}
-          </span>
-          <p className="data__p">{model.description}</p>
-          {/* <button className={`${active === model.id ? "btns__button--active" : ""} btns__button`} /> */}
-        </div>
-      ))}
-    </>
-  );
-};
+import { useState } from "react";
 
-export default Data;
+const Data = ({ models, active, setActive }) => {
+  // const [active, setActive] = useState();
+  
+    return (
+      <>
+        {models.map((model) => (
+          <div
+            key={model.id}
+            className={`${
+              active === model.id ? "escene__data--active" : ""
+            } escene__data`}
+          >
+            <div className="data__close" onClick={()=>setActive(-1)}>
+            <span className="close__span close__span--1"></span>
+            <span className="close__span close__span--2"></span>
+            <span className="close__span close__span--3"></span>
+        </div>
+            <span
+              className="data__span"
+            >
+              {model.title}
+            </span>
+            <p className="data__p">{model.description}</p>
+          </div>
+        ))}
+      </>
+    );
+  };
+  
+  export default Data;
+  
--- src/components/Escene.jsx
+++ src/components/Escene.jsx
@@ -178,7 +178,7 @@ const Escene = () => {
       <Data
           models={models}
           active={active}
-          // setActive={setActive}
+          setActive={setActive}
         />
     </div>
   );
